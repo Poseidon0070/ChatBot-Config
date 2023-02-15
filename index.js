ad6272ee2fe0bb0c792847f5e591b8ea47fa278e
Shikhar <shikharreyya@gmail.com>
2023-02-15 17:34:05 +0530

    config

---
+++ index.js
@@ -0,0 +1,19 @@
+//jshint esversion:6
+const { Configuration, OpenAIApi } = require("openai");
+const prompt = require("prompt-sync")();
+require('dotenv').config()
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+async function runCompletion () {
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: "How are you today?",
+    });
+    console.log(completion.data.choices[0].text);
+}
+runCompletion();
